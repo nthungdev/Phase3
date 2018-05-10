a43627d2a4a91e75e2eaa33b09b53a7f08d5aa42
Sida Zhang <szhan008@plattsburgh.edu>
2018-05-09 22:45:27 -0400

    update insert, select, delete

--- HTML/php/insertmovie.php
+++ HTML/php/insertmovie.php
@@ -6,31 +6,15 @@
 </head>
 <body>
 <?php 
-require('mysqli_connect.php');
+//require('mysqli_connect.php');
 
-$link = @mysqli_connect("localhost", "341group03", "1182E0", "db03");
+$link = @mysqli_connect("localhost", "capmarvel", "thanoswilldie", "mcu");
 mysqli_set_charset($link, 'utf8');
 
 
 if ($link === false){
 	die("ERROR: Could not connect" . mysqli_connect_error());
 }
-
-//$mysql = mysqli_query($dbc, 'INSERT INTO Movies VALUES(titl');
-/*
-    $title = mysqli_real_escape_string($dbc, $_REQUEST['title']);
-    $studio = mysqli_real_escape_string($dbc, $_REQUEST['studio']);
-    $length = mysqli_real_escape_string($dbc, $_REQUEST['length']);
-    $budget = mysqli_real_escape_string($dbc, $_REQUEST['budget']);
-    $revenue = mysqli_real_escape_string($dbc, $_REQUEST['revenue']);
-    $rating = mysqli_real_escape_string($dbc, $_REQUEST['rating']);
-*/
-
-/*
-function checkTitle($title){
-    pass
-}
-*/
 
 $title = $_POST['title'];
 $release_year = $_POST['release_year'];
@@ -47,7 +31,6 @@ $studio = $_POST['studio'];
 $mysql = "INSERT INTO Movie (title, release_year, length, budget, lang, description, rating, revenue, genre, mpaa, studio)
 VALUES ('$title', '$release_year', '$length', '$budget', '$lang', '$description', '$rating', '$revenue', '$genre', '$mpaa', '$studio')";
 
-//mysqli_query($link, $mysql);
 
 if (mysqli_query($link, $mysql)){
     echo "Records added successfully.";
@@ -56,22 +39,6 @@ if (mysqli_query($link, $mysql)){
 }
 mysql_close($link);
 
-/*// Print the submitted information:
-if ( !empty($title) && !empty($release_year) && !empty($lang) 
-    && !empty($description) && !empty($mpaa) ){
-    echo "<p>Submission completed</p>
-        <p>The title is $title </p>
-        <p>The release year is {$release_year} </p>
-        <p>The language is {$lang} </p>
-        <p>The description is {$description} </p>
-        <p>The genre are {$genre[0]} and {$genre[1]} </p>
-        <p>The mpaa rating is {$mpaa} </p>
-
-	mysqli_query($dbc, $mysql)";
-    
-} else { // Missing form value.
-	echo '<p>Please go back and fill out the form again.</p>';
-}*/
 ?>
 </body>
 </html>
