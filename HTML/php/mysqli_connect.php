534e268d8b84ba3879864b05f385b8128f547ec7
Muhtasim Mahir <mmahi001@cslab-154.cs.plattsburgh.edu>
2018-05-16 16:06:22 -0400

    update

--- HTML/php/mysqli_connect.php
+++ HTML/php/mysqli_connect.php
@@ -5,10 +5,11 @@
 // selects the database, and sets the encoding.
 
 // Set the database access information as constants:
-define('DB_USER', 'capmarvel');
-define('DB_PASSWORD', 'thanoswilldie');
+define('DB_USER', '341group03');
+define('DB_PASSWORD', '1182E0');
 define('DB_HOST', 'localhost');
-define('DB_NAME', 'mcu3');
+define('DB_NAME', 'db03');
+
 
 // Make the connection:
 $link = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME) OR die('Could not connect to MySQL: ' . mysqli_connect_error() );
---
+++ HTML/report/groupBy.php
@@ -0,0 +1,64 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Marvel Cinematic Universe</title>
+	<link rel="stylesheet" href="../stylesheet-mcu.css">
+</head>
+<body align="center">
+	<picture>
+		<img src="../images/logo.png" alt="Marvel Cinematic Universe" style="width:500px;height:112px;">
+	</picture>
+	<hr>
+	<div id="nav">
+	<ul>
+		<li><a href="../index.html">Marvel Cinematic Universe</a></li>
+		<li><a href='../nav_search.html'>Search</a></li>
+			<li id='here'><a href='../insert_validate.html'>Insert</a></li>
+			<li><a href='../update_validate.html'>Update</a></li>
+			<li><a href='../delete_validate.html'>Delete</a></li>
+	</div>
+	<hr>
+	<fieldset align="center">
+	<div id="left">
+	<ul>
+
+<?php 
+require "../php/mysqli_connect.php";
+/*
+$link = mysqli_connect("localhost", "capmarvel", "thanoswilldie", "mcu");
+mysqli_set_charset($link, 'utf8');
+
+if ($link === false){
+	die("ERROR: Could not connect" . mysqli_connect_error());
+}
+*/
+
+$id = $_POST['id'];
+$l_name = $_POST['l_name'];
+$m_name = $_POST['m_name'];
+$f_name = $_POST['f_name'];
+$dob = $_POST['dob'];
+$nationality = $_POST['nationality'];
+$gender = $_POST['gender'];
+
+$mysql = "INSERT INTO Person (id, l_name, m_name, f_name, dob, nationality, gender)
+VALUES ('$id', '$l_name', '$m_name', '$f_name', '$dob', '$nationality', '$gender')";
+
+if (mysqli_query($link, $mysql)){
+    echo "Records added successfully.";
+}else{
+    echo "Error: Could not.." . mysqli_error($link);
+}
+mysqli_close($link);
+
+?>
+	</select></label></p>
+		<hr>
+	</ul>
+	</fieldset>
+	<p><input type="submit" name="Insert" value="Next"></p>
+
+</body>
+</html>
+
