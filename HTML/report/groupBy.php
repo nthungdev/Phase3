67d8416e6a515dfb7e971c530e42b7975c84d592
Muhtasim Mahir <mmahi001@cslab-154.cs.plattsburgh.edu>
2018-05-16 16:31:56 -0400

    report added

--- HTML/report/groupBy.php
+++ HTML/report/groupBy.php
@@ -2,63 +2,75 @@
 <html lang="en">
 <head>
 	<meta charset="utf-8">
-	<title>Marvel Cinematic Universe</title>
-	<link rel="stylesheet" href="../stylesheet-mcu.css">
+	<title>Form Feedback</title>
 </head>
-<body align="center">
-	<picture>
-		<img src="../images/logo.png" alt="Marvel Cinematic Universe" style="width:500px;height:112px;">
-	</picture>
-	<hr>
-	<div id="nav">
-	<ul>
-		<li><a href="../index.html">Marvel Cinematic Universe</a></li>
-		<li><a href='../nav_search.html'>Search</a></li>
-			<li id='here'><a href='../insert_validate.html'>Insert</a></li>
-			<li><a href='../update_validate.html'>Update</a></li>
-			<li><a href='../delete_validate.html'>Delete</a></li>
-	</div>
-	<hr>
-	<fieldset align="center">
-	<div id="left">
-	<ul>
-
+<body>
 <?php 
-require "../php/mysqli_connect.php";
-/*
-$link = mysqli_connect("localhost", "capmarvel", "thanoswilldie", "mcu");
+require('mysqli_connect.php');
+
+$link = @mysqli_connect("localhost", "341group03", "1182E0", "db03");
 mysqli_set_charset($link, 'utf8');
+
 
 if ($link === false){
 	die("ERROR: Could not connect" . mysqli_connect_error());
 }
+
+//$mysql = mysqli_query($dbc, 'INSERT INTO Movies VALUES(titl');
+/*
+    $title = mysqli_real_escape_string($dbc, $_REQUEST['title']);
+    $studio = mysqli_real_escape_string($dbc, $_REQUEST['studio']);
+    $length = mysqli_real_escape_string($dbc, $_REQUEST['length']);
+    $budget = mysqli_real_escape_string($dbc, $_REQUEST['budget']);
+    $revenue = mysqli_real_escape_string($dbc, $_REQUEST['revenue']);
+    $rating = mysqli_real_escape_string($dbc, $_REQUEST['rating']);
 */
 
-$id = $_POST['id'];
-$l_name = $_POST['l_name'];
-$m_name = $_POST['m_name'];
-$f_name = $_POST['f_name'];
-$dob = $_POST['dob'];
-$nationality = $_POST['nationality'];
-$gender = $_POST['gender'];
+/*
+function checkTitle($title){
+    pass
+}
+*/
 
-$mysql = "INSERT INTO Person (id, l_name, m_name, f_name, dob, nationality, gender)
-VALUES ('$id', '$l_name', '$m_name', '$f_name', '$dob', '$nationality', '$gender')";
+$title = $_POST['title'];
+$release_year = $_POST['release_year'];
+$length = $_POST['length'];
+$budget = $_POST['budget'];
+$lang = $_POST['language'];
+$description = $_POST['description'];
+$rating = $_POST['rating'];
+$revenue = $_POST['revenue'];
+$genre = $_POST['genre'];
+$mpaa = $_POST['mpaa'];
+$studio = $_POST['studio'];
+
+SELECT title, revenue AVG  FROM Movie m WHERE m.revenue > (SELECT Avg(revenue) FROM Movie);
+SELECT id, movie_title From Position WHERE id LIKE 'A%' ORDER BY id;
+
+$mysql = "INSERT INTO Superhero VALUES ('$alias', '$sup_name', '$id', '$gender', '$species', '$ability')";
 
 if (mysqli_query($link, $mysql)){
     echo "Records added successfully.";
 }else{
-    echo "Error: Could not.." . mysqli_error($link);
+    echo "Error: Could not.." . mysql_error($link);
 }
-mysqli_close($link);
+mysql_close($link);
 
+/*
+// Print the submitted information:
+if ( !empty($title) && !empty($release_year) && !empty($lang) 
+    && !empty($description) && !empty($mpaa) ){
+    echo "<p>Submission completed</p>
+        <p>The title is $title </p>
+        <p>The release year is {$release_year} </p>
+        <p>The language is {$lang} </p>
+        <p>The description is {$description} </p>
+        <p>The genre are {$genre[0]} and {$genre[1]} </p>
+        <p>The mpaa rating is {$mpaa} </p>";
+    
+} else { // Missing form value.
+	echo '<p>Please go back and fill out the form again.</p>';
+}*/
 ?>
-	</select></label></p>
-		<hr>
-	</ul>
-	</fieldset>
-	<p><input type="submit" name="Insert" value="Next"></p>
-
 </body>
 </html>
-
