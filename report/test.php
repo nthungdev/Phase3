9444c6c42237e68acbb14615e8e5d33c2faaac3d
Hung Nguyen <nthungdev@gmail.com>
2018-05-16 20:57:10 -0400

    update test

--- report/test.php
+++ report/test.php
@@ -40,7 +40,40 @@ $avg_revenue = $row['avg'];
 $result = $link->query($query1);
 
 echo "The average revenue is " . round($avg_revenue, 0) . "<br> <br>";
+echo "<p>These are the movies with revenue higher than the average revenue: </p> <br> <br>";
 
+if ($result) {
+    if (mysqli_num_rows($result) > 0) {
+        // output data of each row
+        while($row = $result->fetch_assoc()) {
+            echo $row['title'] . " | $" . $row['revenue'] . "<br>";
+    
+            $actor_query = "select l_name, f_name, movie_title from Position p1, Person p2
+            where p1.movie_title = '{$row['title']}' and p1.id like 'A0%' and p1.id = p2.id";
+            
+            $result2 = $link->query($actor_query);
+            if ($result2){
+                if ($result2->num_rows > 0) {
+                    while($row2 = $result2->fetch_assoc()) {
+                        echo "Cast: " . $row2['f_name'] . " " . $row2['l_name'] . "<br>";
+                    }
+                    echo "<br>";
+                }
+                else {
+                    echo "No actors enrolled in this movie!?";
+                    echo "<br> <br>";
+                }
+            }
+            else {
+                echo "No actors enrolled in this movie!?";
+                echo "<br> <br>";
+            }
+        }
+    } 
+    else {
+        echo "0 results";
+    }
+}
 if (mysqli_num_rows($result) > 0) {
     // output data of each row
     while($row = $result->fetch_assoc()) {
@@ -50,15 +83,21 @@ if (mysqli_num_rows($result) > 0) {
         where p1.movie_title = '{$row['title']}' and p1.id like 'A0%' and p1.id = p2.id";
         
         $result2 = $link->query($actor_query);
-        if ($result2->num_rows > 0) {
-            while($row2 = $result2->fetch_assoc()) {
-                echo "Actor: " . $row2['f_name'] . "<br>";
+        if ($result2){
+            if ($result2->num_rows > 0) {
+                while($row2 = $result2->fetch_assoc()) {
+                    echo "Actor: " . $row2['f_name'] . "<br>";
+                }
             }
         }
+        else {
+            echo "No actors enrolled in this movie!?";
+            echo "<br>";
+        }
         
-        
-
+        echo "<br>";
     }
+
 } else {
     echo "0 results";
 }
